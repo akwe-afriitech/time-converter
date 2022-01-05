b404cd79551ad5bc59a243afa41ef9530fb71f99
Earl Millen Toh Akwe <69284908+akwe-afriitech@users.noreply.github.com>
2022-01-05 07:04:52 +0100

    Update converter.java

--- converter.java
+++ converter.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class Program {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
     
+    Scanner scanner = new Scanner(System.in);
     int days = scanner.nextInt();
     long seconds = days * 24 * 60 * 60;
     System.out.println( seconds " seconds ");
+    
   }
 }
